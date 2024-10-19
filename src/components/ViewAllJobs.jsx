0c48196eaabb6569f9e1f14478231898aa9e06a8
DilkiSewwwandi <2021cs185@stu.ucsc.cmb.ac.lk>
2024-10-19 16:36:07 +0530

    Routes

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -1,14 +1,15 @@
-
+import { Link } from 'react-router-dom';
 const ViewAllJobs = () => {
     return (
         <section class="m-auto max-w-lg my-10 px-6">
-            <a
-                href="jobs.html"
-                class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-            >View All Jobs</a
-            >
+            <Link
+                to='/jobs'
+                class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+            View All Jobs
+            </Link>
+            
         </section>
     );
 };
 
-export default ViewAllJobs
+export default ViewAllJobs;
